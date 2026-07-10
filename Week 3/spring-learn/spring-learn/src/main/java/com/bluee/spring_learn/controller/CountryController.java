c58aa8b657c96e21b4ea6b45f11b67a2a34b50e4
Nivashini Kumar <nivashini1910@gmail.com>
2026-07-10 22:19:08 +0530

    Exercise 2: Get Country REST Service completed

--- Week 3/spring-learn/spring-learn/src/main/java/com/bluee/spring_learn/controller/CountryController.java
+++ Week 3/spring-learn/spring-learn/src/main/java/com/bluee/spring_learn/controller/CountryController.java
@@ -1,23 +1,18 @@
 package com.bluee.spring_learn.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.bluee.spring_learn.model.Country;
+import com.bluee.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CountryController {
 
-    @GetMapping("/country")
-    public Country getCountry() {
+    @Autowired
+    private CountryService countryService;
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        return country;
+    @RequestMapping(value = "/country/{code}", method = RequestMethod.GET)
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
     }
 }
