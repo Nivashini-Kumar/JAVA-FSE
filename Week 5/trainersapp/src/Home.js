b0a624581884e4d9f7abc897f8ad51951b7894e3
Nivashini Kumar <nivashini1910@gmail.com>
2026-07-24 00:01:19 +0530

    Week 5 - Exercise 6: React Router Navigation

---
+++ Week 5/trainersapp/src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Cognizant Trainer Management Portal</h2>
+            <p>Select Trainers from the navigation menu.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ Week 5/trainersapp/src/TrainersMock.js
@@ -0,0 +1,46 @@
+import Trainer from "./Trainer";
+
+const TrainersData = [
+    new Trainer(
+        1,
+        "Elisa Smith",
+        "elisa@cognizant.com",
+        "9876543210",
+        "Java FSD",
+        "Java, Spring Boot, React"
+    ),
+    new Trainer(
+        2,
+        "John Doe",
+        "john@cognizant.com",
+        "9876543211",
+        ".NET FSD",
+        "C#, ASP.NET, SQL Server"
+    ),
+    new Trainer(
+        3,
+        "Emma Swan",
+        "emma@cognizant.com",
+        "9876543212",
+        "Python",
+        "Python, Django, Flask"
+    ),
+    new Trainer(
+        4,
+        "Marie Curie",
+        "marie@cognizant.com",
+        "9876543213",
+        "Data Science",
+        "Python, Pandas, Machine Learning"
+    ),
+    new Trainer(
+        5,
+        "Jojo Jose",
+        "jojo@cognizant.com",
+        "9876543214",
+        "React",
+        "React, JavaScript, HTML, CSS"
+    )
+];
+
+export default TrainersData;
